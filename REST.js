5d079c8657aae91de8e0be5debf0dcbc387352c8
Кирилл <kosarsMK@gmail.com>
2020-11-01 15:31:27 +0200

    Update REST.js

--- REST.js
+++ REST.js
@@ -5,21 +5,24 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {
             'Content-type' : 'text/html'
         })
-
         res.end("<a href='/users'>Proceed to students</a>")
     }
     else if(req.url == '/users') {
         res.writeHead(200, {
             'Content-type' : 'text/json'
         })
-        
         const users = [
             {name: "Sasha", age: 18},
             {name: "Masha", age: 18}
         ]
-
         res.end(JSON.stringify(users));
     }
+    else {
+        res.writeHead(200, {
+            'Content-type' : 'text/html'
+        })
+        res.end("<h1>404 Page not found :(</h1><br><a href='/'>Home</a>")
+    } 
 })
 
 server.listen(3000, () => {
